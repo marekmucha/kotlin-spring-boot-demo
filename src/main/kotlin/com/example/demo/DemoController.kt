dc69a69ecc8e5b959890f3c8f0d7a1c485d9c90f
Marek Mucha <>
2022-03-24 16:18:57 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/demo/DemoController.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DemoController {
+
+    @GetMapping("/demo")
+    fun demo() = "Hello World"
+}
